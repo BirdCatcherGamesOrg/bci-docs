1acdfe3d50482489b54c0bdbce71c5c36a729eea
BirdCatcherGames <birdcatchergames@gmail.com>
2025-05-02 15:10:17 -0700

    Removing files checked in by the mkdocs process

--- bci-ue-logger-lib/Public/Macros/SelfFilter.h
+++
@@ -1,9 +0,0 @@
-﻿// SPDX-License-Identifier: MPL-2.0
-
-#pragma once
-
-#if defined(WITH_SPDLOG)
-#define _BCICORELOG_SELF_FILTER "[BCILOG] "
-#else
-#define _BCICORELOG_SELF_FILTER ""
-#endif // WITH_SPDLOG
--- bcivndk-extensions-ue-plugin/Source/BCIVNDKDialogue/Private/BCIVNDKDialogueDeveloperSettings.cpp
+++
@@ -1,4 +0,0 @@
-﻿// // SPDX-License-Identifier: MPL-2.0
-
-
-#include "BCIVNDKDialogueDeveloperSettings.h"
