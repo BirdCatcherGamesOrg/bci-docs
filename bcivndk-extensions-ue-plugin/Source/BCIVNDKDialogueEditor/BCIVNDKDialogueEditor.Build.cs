e6c6a38fc3dbb33d502404281f5ac0400d4d2bbc
BirdCatcherGames <birdcatchergames@gmail.com>
2025-05-02 15:00:09 -0700

    Deployed 1c3e744 with MkDocs version: 1.6.1

---
+++ bcivndk-extensions-ue-plugin/Source/BCIVNDKDialogueEditor/BCIVNDKDialogueEditor.Build.cs
@@ -0,0 +1,31 @@
+﻿using UnrealBuildTool;
+
+public class BCIVNDKDialogueEditor : ModuleRules
+{
+    public BCIVNDKDialogueEditor(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                "BCIVNDKDialogue",
+                "MovieScene",
+                "MovieSceneTracks",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "BCIWidgetEditor",
+                "CoreUObject",
+                "Engine",
+                "Sequencer",
+                "Slate",
+                "SlateCore",
+            }
+        );
+    }
+}
